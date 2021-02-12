2fea5cd194d753b947f0169dd015384f054ca6f6
tech-hack-rich <ricardoathleta@icloud.com>
2021-02-12 19:02:49 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,26 @@
 class Item < ApplicationRecord
-  with_options presence:true do
+  with_options presence: true do
     validates :name
     validates :info
+    validates :price
+  end
+
+  belongs_to :user
+  has_one_attached :image
+  validate :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
+  with_options presence: true, numericality: { other_than: 0 } true do
     validates :category_id
     validates :condition_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipping_time_id
-    validates :price
   end
-  belongs_to :user
-  has_one_attached :image
 end
