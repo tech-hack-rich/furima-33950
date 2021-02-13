b825fea51ca7c433d468a5d0414adb30d281cbd1
tech-hack-rich <ricardoathleta@icloud.com>
2021-02-13 18:13:33 +0900

    レビュー修正１回目

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     delivery_fee_id {1}
     prefecture_id {1}
     shipping_time_id {1}
-    price {"5000"}
+    price {5000}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,13 +74,13 @@ RSpec.describe Item, type: :model do
       end
 
       it 'priceが¥300~¥9,999,999の間でないと出品できないこと(300円未満のとき)' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
 
       it 'priceが¥300~¥9,999,999の間でないと出品できないこと(10,000,000円以上のとき)' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
